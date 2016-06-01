b86658b839e173f000d3568995bec42d3e49d141
Bernard Notarianni <bernard.notarianni@gmail.com>
2016-06-01 23:56:20 +0200

    first step

--- step1.elm
+++ step1.elm
@@ -1,4 +1,27 @@
-import Html exposing (text)
+import Html exposing (text, div)
+import Color exposing (Color, lightBlue, purple, blue, black, white)
+import Element exposing (Element, toHtml, color, container, topLeftAt, centered, absolute)
+import Text exposing (Text)
+
+what = "Display a blue box containing text 'hello'"
 
 main =
-  text "Hello, World!"
+    div [] 
+    [ text what
+    , view
+    ]
+    
+view =
+  let
+    (width, height) = (500, 500)
+    position = topLeftAt (absolute 50) (absolute 20)
+  in
+    toHtml
+      <| color blue
+      <| container width height position
+      <| centered
+      <| coloredText "hello" white
+
+
+coloredText : String -> Color -> Text
+coloredText text color = (Text.color color (Text.fromString text))
