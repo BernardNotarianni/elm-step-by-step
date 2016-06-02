276bdd6fc2c3fde0ce45de5a716313e87fd9c389
Bernard Notarianni <bernard.notarianni@gmail.com>
2016-06-02 10:41:17 +0200

    step 2

---
+++ step2.elm
@@ -0,0 +1,29 @@
+import Html exposing (div)
+import Element exposing (Element, toHtml)
+
+import Color exposing (..)
+import Collage exposing (..)
+
+what = "Display a blue box containing text 'hello'"
+
+main =
+    div [] 
+    [ Html.text what
+    , view
+    ]
+    
+view =
+    toHtml canvas
+
+canvas =
+  let
+    canvasSize = 500
+  in
+    collage canvasSize canvasSize
+      [ rect 10 10 |> filled red |> moveXY (250,250)
+      , rect 10 10 |> filled blue |> moveXY (20,50)
+      ]
+      
+moveXY : (Int, Int) -> Form -> Form
+moveXY (x, y) =
+  move ( x |> toFloat , y |> toFloat )
